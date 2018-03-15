9900bd368174bdf9c269dc113309a5e890f44084
DESKTOP-OSN7PRF\king <kingcheeng163@gmail.com>
2018-03-15 14:14:24 +0800

    工厂模式的Java api栗子

--- src/factory_method/Car.java
+++
@@ -1,7 +0,0 @@
-package factory_method;
-
-/**
- * Created by king on 2018/3/15.
- */
-public class Car {
-}
--- src/factory_method/CarBuyer.java
+++ src/factory_method/CarBuyer.java
@@ -2,6 +2,11 @@ package factory_method;
 
 import simple_factory.BrandSpeedEnum;
 
+import java.net.URLStreamHandlerFactory;
+import java.nio.charset.Charset;
+import java.text.NumberFormat;
+import java.util.EnumSet;
+
 /**
  * Created by king on 2018/3/15.
  */
@@ -9,5 +14,10 @@ public class CarBuyer {
     public static void main(String[] args) {
         CarShop carShop = new BeijingCarShop();
         carShop.sellCar(BrandSpeedEnum.BENZ,CarType.ROADSTER);
+
+//        NumberFormat
+//        Charset
+//        URLStreamHandlerFactory
+//        EnumSet
     }
 }
